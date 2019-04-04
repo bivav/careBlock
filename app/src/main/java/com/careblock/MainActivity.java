11c21e4595ad6fb93dd6f77075d663a376e091f7
akshysatish <31073850+akshysatish@users.noreply.github.com>
2019-04-04 15:39:50 +0530

    ui implemented

--- app/src/main/java/com/careblock/MainActivity.java
+++ app/src/main/java/com/careblock/MainActivity.java
@@ -1,7 +1,11 @@
 package com.careblock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView userButton = findViewById(R.id.imageView2);
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
