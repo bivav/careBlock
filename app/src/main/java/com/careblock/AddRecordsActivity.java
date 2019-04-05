5d6239c5dfef6a482306abacd500581fcf783fac
akshysatish <31073850+akshysatish@users.noreply.github.com>
2019-04-05 09:28:40 +0530

    ui done

--- app/src/main/java/com/careblock/AddRecordsActivity.java
+++ app/src/main/java/com/careblock/AddRecordsActivity.java
@@ -2,6 +2,9 @@ package com.careblock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddRecordsActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class AddRecordsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_records);
+
+        EditText name = findViewById(R.id.editText);
+        EditText id = findViewById(R.id.editText2);
+        EditText password = findViewById(R.id.editText3);
+        EditText summary = findViewById(R.id.editText4);
+
+        Button submit = findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
